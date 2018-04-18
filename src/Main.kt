5eb2456ef2453f815392f0f3e3391e79e0e33223
JorgeLuis1395 <carrillojorge1395@hotmail.com>
2018-04-18 09:55:37 -0500

    Creación de Funciones

---
+++ src/Main.kt
@@ -0,0 +1,8 @@
+fun sumarDosNumeros(numeroUno: Int, numeroDos: Int):Int{
+    return numeroUno+numeroDos
+}
+
+fun main(args: Array<String>){
+    println("Hola Mundo!") //remplaza
+    print(sumarDosNumeros(7,8))
+}
